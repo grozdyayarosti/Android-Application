fe80117cbcbe280252ed86cbd997a07c61a45277
grozdyayarosti <Kurtzouma1>
2022-10-12 19:08:47 +0500

    Тестовое приложение

--- app/src/main/java/ru/permexample/objectsobmap/CActivityMain.kt
+++ app/src/main/java/ru/permexample/objectsobmap/CActivityMain.kt
@@ -3,10 +3,14 @@ package ru.permexample.objectsobmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class CActivityMain : AppCompatActivity()
 {
+    val obj = CObject("It's name","It's description")
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -14,8 +18,13 @@ class CActivityMain : AppCompatActivity()
         setContentView(R.layout.activity_cmain)
 
         val button : Button = findViewById(R.id.button1)
+        val editTextInput : EditText = findViewById(R.id.editTextInput)
+        val textView2 : TextView = findViewById(R.id.textView2)
         button.setOnClickListener{
-            Toast.makeText(applicationContext, "Hello!", Toast.LENGTH_SHORT).show()
+            obj.comments.add(editTextInput.text.toString())
+
+//            Toast.makeText(applicationContext, "Result: ${obj.comments.size}!", Toast.LENGTH_SHORT).show()
+            textView2.text = "" + obj.comments.size
         }
     }
 }
